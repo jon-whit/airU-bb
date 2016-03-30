62e9842dd2ea1b21ac26a30def96854592c7beb2
Taylor Wilson <rolyat.wilson@gmail.com>
2016-03-30 13:06:46 -0600

    Adds device registration script and MACAddress retrieval.
      - url needs to be updated when server endpoint is implemented
      - to test: ensure MACAddress retrieval is accurate and
                 does not change on BB

--- register-station.py
+++ register-station.py
@@ -1,5 +1,21 @@
+from lib.airu.macaddress import MacAddress
+import requests
+import pprint
 
 if __name__ == '__main__':
+  """
+  Uploads the MACAddress to the server for station registration.
+  """
+  mac = MacAddress()
 
-    # TODO: Fill this script out!
-    print "register-station.py"
+  # TODO: remove 'dev' from URL when API is re-deployed to PROD
+  # TODO: replace ???*??? with endpoint once it is implemented
+  url     = 'http://dev.air.eng.utah.edu/api/stations/???waiting_for_endpoint???'
+  headers = {'Content-Type': 'application/json'}
+  message = {"DeviceID": mac.addr}
+
+  r = requests.post(url, data=json.dumps(message), headers=headers)
+  if r.status_code == 200:
+    print 'Device Registered.'
+  else:
+    print 'A server error occurred.'

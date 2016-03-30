522447e566c2ae98c45cc6d479d277e5ade92f77
Taylor Wilson <rolyat.wilson@gmail.com>
2016-03-30 15:17:54 -0600

    Adds mock data script to populate non-root level DB. Adds functionality to data-upload.
      - To Test:
         - make sure MACAddress is registered with server
            OR hardcode { "Id": "T1000" } as Station value in upload JSON
         - run mock_data.py
         - remove '/root/' from db.init in data-upload.py
         - run data-upload
    
         - when API is fixed, this should print 200

--- data-upload.py
+++ data-upload.py
@@ -1,5 +1,81 @@
+import json
+import requests
+from lib.airu.dbmodels import *
+from lib.airu.utils import get_mac
+
+def prepare_db():
+  """
+  Establishes a connection with the database.
+  """
+  # remove '/root/' when testing with mock_data
+  db.init('/root/air-metrics.db')
+  db.connect()
+
+def fetch_data():
+  """
+  Queries local database for all metrics which have not been uploaded to AirU server.
+
+  :return: An array of all data metrics which have not been uploaded.
+  """
+  return AirMeasurement().select().where(~(AirMeasurement.uploaded))
+
+def encode_data(metrics):
+  """
+  Encodes an array of data metrics into a dictionary which can be used as JSON data.
+  Below is the model expected by the server API.
+    [{
+        "Time": "12-31-2015",
+        "Station": {
+            "Id": "T1000"
+        },
+        "Parameter": {
+            "Name": "PM2.5",
+            "Unit": "UG/M3"
+        },
+        "Location": {
+            "lat": 40.687033,
+            "lng": -111.824517
+        },
+        "Value": 30
+    }]
+  """
+  msg = []
+  for m in metrics:
+    msg.append({
+      "Time": str(m.date_added),
+      "Station": { "Id": get_mac('eth0') },
+      "Parameter": { "Name": m.type, "Unit": m.unit },
+      "Location": { "lat": m.latitude, "lng": m.longitude },
+      "Value": m.value
+      })
+
+  return msg
+
+def upload(message, metrics):
+  """
+  Uploads JSON messages to AirU central server API.
+  """
+  
+  url     = 'http://dev.air.eng.utah.edu/api/stations/data'
+  headers = {'Content-Type': 'application/json'}
+
+  print json.dumps(message)
+
+  r = requests.post(url, data=json.dumps(message), headers=headers)
+  print r.status_code # TODO: just printing for sanity check
+  
+  if r.status_code == 200:
+    print 'OK! SUCCESS'
+    for m in metrics:
+      m.uploaded = True
+      m.save()
 
 if __name__ == '__main__':
-
-    # TODO: Fill this script out!
-    print "data-upload.py"
+  """
+  Entry point for uploaded Beaglebone data to server API.
+  """
+  prepare_db()
+  metrics = fetch_data()
+  if len(metrics) > 0:
+    # only upload if there is something to upload
+    upload(encode_data(metrics), metrics)
---
+++ mock_data.py
@@ -0,0 +1,47 @@
+import json
+import requests
+from lib.airu.dbmodels import *
+import datetime
+import math
+import random
+import pprint
+
+if __name__ == '__main__':
+  # use non-root level DB for testing
+  db.init('air-metrics.db')
+  db.connect()
+  db.create_tables([AirMeasurement], True)
+
+  lat = 40.687033
+  lon = -111.824517
+  altitude = 1432
+
+  time = datetime.datetime.now()
+  date = datetime.timedelta(seconds=3155690)
+  time = time - date
+
+  for i in range(1, 2):
+    date = datetime.timedelta(seconds=9000)
+    time = time + date
+
+    pm1  = abs(math.sin(i) + random.uniform(0.1,23))
+    pm25 = abs(math.cos(i) + random.uniform(0.1, 17))
+    pm10 = abs(math.asinh(i) + random.uniform(0.1, 32))
+    temp = abs(math.sin(i) + random.uniform(0.1, 1))
+    humidity = abs(math.asinh(i) + random.uniform(0.1, 1))
+    pressure = abs(math.sin(i) + random.uniform(0.1, 1))
+
+    measurement = AirMeasurement(type='Temperature', value=temp, unit='C', latitude=lat, longitude=lon)
+    measurement.save()
+    measurement = AirMeasurement(type='Humidity', value=humidity, unit='%', latitude=lat, longitude=lon)
+    measurement.save()
+    measurement = AirMeasurement(type='Pressure', value=pressure, unit='Pascal', latitude=lat, longitude=lon)
+    measurement.save()
+    measurement = AirMeasurement(type='Altitude', value=altitude, unit='M', latitude=lat, longitude=lon)
+    measurement.save()
+    measurement = AirMeasurement(type='PM1.0', value=pm1, unit='UG/M3', latitude=lat, longitude=lon)
+    measurement.save()
+    measurement = AirMeasurement(type='PM2.5', value=pm25, unit='UG/M3', latitude=lat, longitude=lon)
+    measurement.save()
+    measurement = AirMeasurement(type='PM10.0', value=pm10, unit='UG/M3', latitude=lat, longitude=lon)
+    measurement.save()

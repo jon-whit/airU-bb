fc5e58063ce2e48f7531eb5ae120555ea0fbac5c
Taylor Wilson <rolyat.wilson@gmail.com>
2016-03-30 15:32:48 -0600

    Add registration functionality.
      - modify url value when server api endpoint is implemented

--- register-station.py
+++ register-station.py
@@ -1,5 +1,18 @@
+import json
+import requests
+from lib.airu.utils import get_mac
 
 if __name__ == '__main__':
+  """
+  Uploads the MACAddress to the server for station registration.
+  """
 
-    # TODO: Fill this script out!
-    print "register-station.py"
+  url     = 'http://dev.air.eng.utah.edu/api/stations/ping'
+  headers = {'Content-Type': 'application/json'}
+  message = {"DeviceID": get_mac('eth0')}
+
+  r = requests.post(url, data=json.dumps(message), headers=headers)
+  if r.status_code == 200:
+    print 'Device Registered.'
+  else:
+    print 'A server error occurred.'
